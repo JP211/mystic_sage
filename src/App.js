7daeee773b78f50c75d951f71015b34eff459900
Jessica Cockerell <jessica@MacBook-Air.local>
2019-11-30 23:18:23 -0600

    Added header component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import logo from './MysticSage_logo_black.png';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
-    <img src={logo} className="App-logo" alt="logo" />
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
