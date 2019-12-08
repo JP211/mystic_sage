01f27c5d1847e65147d367c3f415ab0335a43a25
Jessica Cockerell <jessica@MacBook-Air.local>
2019-12-07 22:26:30 -0600

    Cart dropdown hidden on click + styling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <img src={logo} className='logo' alt='logo'/>
@@ -35,12 +35,13 @@ const Header = ({ currentUser }) => (
           )}
             <CartIcon />
       </div>
-          <CartDropdown />
+          {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
