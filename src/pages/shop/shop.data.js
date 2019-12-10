016b7362138c44b0c44e14887a669b11d3d86d0a
Jessica Cockerell <jessica@MacBook-Air.local>
2019-12-09 18:40:21 -0600

    Cart quantity fucntion

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -72,61 +72,61 @@ const SHOP_DATA = [
     routeName: 'candles',
     items: [
       {
-        id: 1,
+        id: 11,
         name: 'Glass Tealight Set',
         imageUrl: 'https://i.imgur.com/w4NodZH.jpg',
         price: 20
       },
       {
-        id: 2,
+        id: 12,
         name: 'Zen Garden Candle',
         imageUrl: 'https://i.imgur.com/duxf3IL.jpg',
         price: 28
       },
       {
-        id: 3,
+        id: 13,
         name: 'Bamboo Blossom Candle',
         imageUrl: 'https://i.imgur.com/YoR0pGJ.jpg',
         price: 26
       },
       {
-        id: 4,
+        id: 14,
         name: 'Stone Essence Oil Candle',
         imageUrl: 'https://i.imgur.com/z7KZxJT.jpg',
         price: 55
       },
       {
-        id: 5,
+        id: 15,
         name: 'Carved Rose Candle',
         imageUrl: 'https://i.imgur.com/fqWiF7L.jpg',
         price: 59
       },
       {
-        id: 6,
+        id: 16,
         name: 'Tealight Candle Set',
         imageUrl: 'https://i.imgur.com/l1Jo2mj.jpg',
         price: 11
       },
       {
-        id: 7,
+        id: 17,
         name: 'Beaded Pearl Candle',
         imageUrl: 'https://i.imgur.com/WN7G7NY.jpg',
         price: 36
       },
       {
-        id: 8,
+        id: 18,
         name: 'Cauldron Pot Candle',
         imageUrl: 'https://i.imgur.com/IogxvnS.jpg',
         price: 22
       },
       {
-        id: 9,
+        id: 19,
         name: 'Porcelain Candle Pot Set',
         imageUrl: 'https://i.imgur.com/QKDCYbs.jpg',
         price: 60
       },
       {
-        id: 10,
+        id: 20,
         name: 'Bayberry Pillar Candle Set',
         imageUrl: 'https://i.imgur.com/nBBizUK.jpg',
         price: 40
@@ -139,61 +139,61 @@ const SHOP_DATA = [
     routeName: 'teas',
     items: [
       {
-        id: 1,
+        id: 21,
         name: 'Pumpkin Chai Spice Tea',
         imageUrl: 'https://i.imgur.com/OfvBuBX.jpg',
         price: 25
       },
       {
-        id: 2,
+        id: 22,
         name: 'Oolong Tea Blend',
         imageUrl: 'https://i.imgur.com/8nLRrcV.jpg',
         price: 29
       },
       {
-        id: 3,
+        id: 23,
         name: 'Ginger Spice Tea',
         imageUrl: 'https://i.imgur.com/q3YwaL6.jpg',
         price: 19
       },
       {
-        id: 4,
+        id: 24,
         name: 'Raspberry Hibiscus White Tea',
         imageUrl: 'https://i.imgur.com/VAc96ns.jpg',
         price: 18
       },
       {
-        id: 5,
+        id: 25,
         name: 'Orange Spice Flower Tea Balls',
         imageUrl: 'https://i.imgur.com/jCJaV62.jpg',
         price: 35
       },
       {
-        id: 6,
+        id: 26,
         name: 'Black Tea Dried Fruit Blend',
         imageUrl: 'https://i.imgur.com/X3bb5oS.jpg',
         price: 25
       },
       {
-        id: 7,
+        id: 27,
         name: 'Floral Fruits Berry Tea Blend',
         imageUrl: 'https://i.imgur.com/vh00jkB.jpg',
         price: 35
       },
       {
-        id: 8,
+        id: 28,
         name: 'Twelve Flavors Wood Tea Gift Box',
         imageUrl: 'https://i.imgur.com/9uskvjb.jpg',
         price: 85
       },
       {
-        id: 9,
+        id: 29,
         name: 'Stainless Steel Tea Brew Kettle & Sunflower Tea',
         imageUrl: 'https://i.imgur.com/gpwCAMp.jpg',
         price: 75
       },
       {
-        id: 10,
+        id: 30,
         name: 'Mint Dried Herb Green Tea',
         imageUrl: 'https://i.imgur.com/zs6e3XN.jpg',
         price: 18
@@ -206,61 +206,61 @@ const SHOP_DATA = [
     routeName: 'crystals',
     items: [
       {
-        id: 1,
+        id: 31,
         name: 'Citrine Crystal Geode Slice',
         imageUrl: 'https://i.imgur.com/5xXeROz.jpg',
         price: 25
       },
       {
-        id: 2,
+        id: 32,
         name: 'Amethyst Collector\'s Large Crystal Geode',
         imageUrl: 'https://i.imgur.com/l0lIHM0.jpg',
         price: 200
       },
       {
-        id: 3,
+        id: 33,
         name: 'Crystal Photo Sphere',
         imageUrl: 'https://i.imgur.com/bVM2mYb.jpg',
         price: 45
       },
       {
-        id: 4,
+        id: 34,
         name: 'Meditation White Quartz Crystal',
         imageUrl: 'https://i.imgur.com/BDfSmB9.jpg',
         price: 95
       },
       {
-        id: 5,
+        id: 35,
         name: 'Large Sand Topaz Crystal',
         imageUrl: 'https://i.imgur.com/n9RF2AO.jpg',
         price: 135
       },
       {
-        id: 6,
+        id: 36,
         name: 'Aquamarine Rough Crystal',
         imageUrl: 'https://i.imgur.com/Hd8foMf.jpg',
         price: 80
       },
       {
-        id: 7,
+        id: 37,
         name: 'Amethyst Crystal Burst with Stand',
         imageUrl: 'https://i.imgur.com/8m10TMT.jpg',
         price: 99
       },
       {
-        id: 8,
+        id: 38,
         name: 'Himalayan Salt Crystal Set',
         imageUrl: 'https://i.imgur.com/kSQFlNs.jpg',
         price: 60
       },
       {
-        id: 9,
+        id: 39,
         name: 'Collector\'s Quartz Crystal Formation',
         imageUrl: 'https://i.imgur.com/9qKLUai.jpg',
         price: 185
       },
       {
-        id: 10,
+        id: 40,
         name: 'Mixed Tumbled Crystal Meditation Stones',
         imageUrl: 'https://i.imgur.com/wWeq4w6.jpg',
         price: 25
@@ -273,61 +273,61 @@ const SHOP_DATA = [
     routeName: 'sagekits',
     items: [
       {
-        id: 1,
+        id: 41,
         name: 'Amethyst Cleansing Sage Kit',
         imageUrl: 'https://i.imgur.com/SXZK2uz.png',
         price: 32
       },
       {
-        id: 2,
+        id: 42,
         name: 'Mixed Cleansing Woods Sage Kit',
         imageUrl: 'https://i.imgur.com/c8dq3Tn.jpg',
         price: 20
       },
       {
-        id: 3,
+        id: 43,
         name: 'Asian Berry & Herb Sage Kit',
         imageUrl: 'https://i.imgur.com/X0IEp9E.jpg',
         price: 25
       },
       {
-        id: 4,
+        id: 44,
         name: 'Hickory & Pine Sage Kit',
         imageUrl: 'https://i.imgur.com/1VRjdmv.jpg',
         price: 28
       },
       {
-        id: 5,
+        id: 45,
         name: 'Sage Smudge Sticks',
         imageUrl: 'https://i.imgur.com/AjCpwgC.jpg',
         price: 40
       },
       {
-        id: 6,
+        id: 46,
         name: 'Floral Essence Oils Set',
         imageUrl: 'https://i.imgur.com/EyVKzpN.jpg',
         price: 95
       },
       {
-        id: 7,
+        id: 47,
         name: 'Cleansing Sage & Tarot Set',
         imageUrl: 'https://i.imgur.com/rBJoPYj.jpg',
         price: 25
       },
       {
-        id: 8,
+        id: 48,
         name: 'Brass Purity Incense Burning Kettle',
         imageUrl: 'https://i.imgur.com/pcxUad2.jpg',
         price: 85
       },
       {
-        id: 9,
+        id: 49,
         name: 'Stone & Healing Herbs Mortar + Pestle Kit',
         imageUrl: 'https://i.imgur.com/yKXjJ5P.jpg',
         price: 35
       },
       {
-        id: 10,
+        id: 50,
         name: 'Dried Sage and Cleansing Herb Bundle',
         imageUrl: 'https://i.imgur.com/CNkQpAN.jpg',
         price: 55
