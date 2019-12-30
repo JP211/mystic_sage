5ca1b21cc2d5c225d4c7e6fa8ec026a04c1b9c73
Jessica Cockerell <jessicacockerell@Jessicas-MacBook-Air.local>
2019-12-30 13:48:43 -0700

    Updated header css with styled-components

---
+++ src/components/header/header.styles.jsx
@@ -0,0 +1,41 @@
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const OptionContainerStyles = css`
+  padding: 25px 10px;
+  margin-top: 35px;
+  cursor: pointer;
+`;
+
+export const HeaderContainer = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+  margin-left: 75px;
+`;
+
+export const LogoContainer = styled(Link)`
+  margin-left: -100px;
+  padding-top: 3px;
+`;
+
+export const OptionsContainer = styled.div`
+  width: 50%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-weight: bolder;
+  margin-right: 75px;
+  color: #501882;
+`;
+
+export const OptionLink = styled(Link)`
+  ${OptionContainerStyles}
+`;
+
+export const OptionDiv = styled.div`
+  ${OptionContainerStyles}
+`;
